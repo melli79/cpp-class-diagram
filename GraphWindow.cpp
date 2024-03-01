3b124ed3c31cfd3b937fc9ad72462f3a891ca715
Melchior <melchiorG@gMail.com>
2024-03-01 11:12:35 +0100

    copyright.

--- GraphWindow.cpp
+++ GraphWindow.cpp
@@ -1,6 +1,9 @@
 //
 // Created by Melchior Grützmann on 2024-02-18.
 //
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #include "GraphWindow.h"
 #include <boost/graph/strong_components.hpp>
--- LayoutWorker.cpp
+++ LayoutWorker.cpp
@@ -1,6 +1,10 @@
 //
 // Created by Melchior Grützmann on 2024-02-18.
 //
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 
 #include "LayoutWorker.h"
 #include <boost/graph/fruchterman_reingold.hpp>
